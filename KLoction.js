b6b2b771c6fa56c14bf8f2b0fa35fb11cf59b7e3
karzanOnline <170579160@qq.com>
2016-11-18 11:25:59 +0800

    done

--- KLoction.js
+++ KLoction.js
@@ -2,7 +2,7 @@
  * Created by caozheng on 2016/9/28.
  */
 (function (win) {
-    //demo
+
     //oGetParam("?a=1&b=123")
     var KLocation = function ( url ) {
 
@@ -13,6 +13,7 @@
 
         constructor : KLocation,
 
+        // 检查url是否存在
         checkErr : function () {
             if(!this.url){
                 throw ('error for param from getParam');
@@ -20,11 +21,12 @@
             return true
         },
 
+        // 获取url的协议类型
+        // return Number
         protocolIndex : function (boolean) {
 
-            var re = new RegExp("[\:]","ig");
-            var urlTest = re.exec(this.url);
-
+            var re = new RegExp("[\:]","ig"),
+                urlTest = re.exec(this.url);
             if(boolean){
                 //只是检测是否有协议类型
                 if(!urlTest){
@@ -47,49 +49,40 @@
 
         this.url = arguments[0];
         this.protocol = this.getProtocol();
-        this.host = this.getHost();
-
+        this.host = this.hostname = this.getHost();
+        this.port = this.getPort();
+        this.pathname = this.getPathname();
     };
     init.prototype = KLocation.fn;
 
-    //返回json
-    //url后面的参数json化
+    // url参数json化
+    // return Object
     init.prototype.getParam = function () {
-        KLocation.fn.checkErr.call(this);
 
+        KLocation.fn.checkErr.call(this);
         var re = new RegExp("([?=&])([A-Za-z0-9]*)","ig"),
             rel = new RegExp("[\"\']","ig");
-        arr = [],
+            arr = [],
             result = {},
             url = this.url.replace(rel,""); //去除" ' 的影响
-
         //to array
         while(value=re.exec(url)){
-
             if(value[2]==''){
                 continue;
             }
-
             arr.push(value[2]);
         }
-
         //to json
         for (var i=0;i<arr.length;i++){
-            if(i%2==0){
-                //even property
-                result[arr[i]] = ''
 
-            }else{
-                //odd value
-                result[arr[i-1]] = arr[i]
-            }
+            (i%2 == 0) ? (result[arr[i]] = '') : (result[arr[i-1]] = arr[i])
         }
 
         return result
     };
 
-    //返回string
-    //protocol
+    // 返回协议类型
+    // return string
     init.prototype.getProtocol = function () {
         //日常判断url是否存在
         KLocation.fn.checkErr.call(this);
@@ -97,23 +90,73 @@
         return this.url.slice(0, KLocation.fn.protocolIndex.call(this,false))
     };
 
+    // 返回host
+    // return String
     init.prototype.getHost = function () {
-
+        // 检测
         KLocation.fn.checkErr.call(this);
-        var re = new RegExp("(\.)","ig");
+        var re = /(\.([a-zA-z])+)/ig;
+        var last;
+        // 获取协议索引
         var index = KLocation.fn.protocolIndex.call(this,true);
-        var i = 0;
-        re.exec(this.url);
-        var last =  re.exec(this.url).index;
-
-        //检查如果http:// 写成http:/ 时只需将/全局匹配掉
+        if(index){
+            index = index + 2;
+        }
+        while(reLast = re.exec(this.url)){
+            if(reLast){
+                last = reLast.index + reLast[0].length
+            }
+        }
+        // 检查如果http:// 写成http:/ 时只需将/全局匹配掉
         var host =  this.url.slice(index, last).replace(/[/]/ig,'');
         return host
     };
 
+    // 是否具有协议的类型
+    // return Number
+    init.prototype.getDeal = function () {
+        // 匹配协议类型
+        var re = new RegExp('(:\/\/)|(\/\/)',"ig"),
+            url = this.url,
+            result = re.exec(url),
+            index;
+        if (result){
+            index = result[0].length + result.index;
+            return index
+        }
+        return 0
+    };
+
+    // 获取端口号
+    // return Number
+    init.prototype.getPort = function () {
+        var re = /:([0-9])+/ig,
+            url = this.url,
+            result = re.exec(url);
+
+        if(result){
+            return result[0]
+        }
+        // http 默认80端口
+        // https 默认443端口
+
+        return 0
+    };
+
+    // 获取pathname
+    // return String
+    init.prototype.getPathname = function () {
+        var _this = this,
+            url = _this.url,
+            result = _this.getProtocol() + "//" + _this.getHost();
+        // 去掉后面的参数
+        url = url.substring(0, url.indexOf('?'));
+        return url.substring(result.length, url.length) || '/'
+    };
+
     win.KLocation = KLocation;
 
-    console.log(KLocation('http://www.baidu.com?"a"="1"&b=2'));
+    // console.log(KLocation('http://www.baidu.com?"a"="1"&b=2'));
 
 
 })(window);
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-**url-location**
+**KLocation**
 
 使用正则匹配url模拟返回window.location的值
 
@@ -14,24 +14,31 @@
         url:"http://www.baidu.com?a=1&b=2",
         host : "www.baidu.com",
         protocol : "http:",
-        pathname : //待添加,
-        search : //待添加,
-        port :  //待添加
-        hostname : //待添加
+        pathname : "/",
+        port :  ''
+        hostname : "www.baidu.com"
         }
         
 方法：
 
 返回json化参数
     
-    KLocation().getParam() // {a:1,b:2}
+    KLocation(url).getParam() // {a:1,b:2}
     
 返回host
 
-    KLocation().getHost() 
+    KLocation(url).getHost() 
 
 返回Protocol
 
-    KLocation()。getProtocol()
+    KLocation(url).getProtocol()
     
-...待我回家写完
+返回port
+    
+    KLocation(url).getPort()
+    
+返回pathname
+    
+    KLocation(url).getPathname()
+    
+    
